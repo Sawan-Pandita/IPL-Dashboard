505775ca59c492c9c20b69316b5e70186cfb8302
Sawan <sawanpandita21@gmail.com>
2022-01-21 11:18:41 +0530

    application is ready with frontend and backend api

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
+import './App.scss';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/teams/:teamName" element={<TeamPage />} />
           <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
   );
